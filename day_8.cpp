82ce036026ddf34632d44cbe792b1085708efb90
Muhammad Mahgoub <35633070+MuhammadAbdelmoneim@users.noreply.github.com>
2020-04-08 22:55:17 +0200

    Middle of the Linked List problem

---
+++ day_8.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head->next == NULL)
+            return head;
+        if(head->next->next == NULL)
+            return head->next;
+        map<int , ListNode*>m;
+        ListNode *current = head;
+        int count  = 1;
+        while(current->next != NULL)
+        {
+            m.insert(std::make_pair(count , current)); 
+            current = current->next;
+           count++;
+            
+        }
+            return m.at((count / 2) + 1);
+
+    }
+};
